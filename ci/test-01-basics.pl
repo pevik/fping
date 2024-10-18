9abdb18e63641abb37816c18a940fb0bc5768ced
Petr Vorel <petr.vorel@gmail.com>
2024-10-18 20:48:08 +0200

    ping: Add support for IPv4-mapped IPv6 address format
    IP addresses with ::ffff: prefix are IPv6 addresses which are mapped to
    IPv4. Therefore ping resulting IPv4 address.
    
        $ ./src/fping -c1 ::ffff:127.0.0.1
        IPv4-Mapped-in-IPv6 address, using IPv4 127.0.0.1
         [<- 127.0.0.1]127.0.0.1        : [0], 64 bytes, 0.066 ms (0.066 avg, 0% loss)
    
    Signed-off-by: Petr Vorel <petr.vorel@gmail.com>

--- ci/test-01-basics.pl
+++ ci/test-01-basics.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 
-use Test::Command tests => 15;
+use Test::Command tests => 18;
 use Test::More;
 
 # ping 127.0.0.1
@@ -33,6 +33,18 @@ SKIP: {
     $cmd->exit_is_num(0);
     $cmd->stdout_is_eq("ff02::1 is alive\n");
     $cmd->stderr_like(qr{ \[<- .*\]});
+}
+
+# ping ::ffff:127.0.0.1
+SKIP: {
+    if($ENV{SKIP_IPV6}) {
+        skip 'Skip IPv6 tests', 3;
+    }
+    my $cmd = Test::Command->new(cmd => "fping ::ffff:127.0.0.1");
+    $cmd->exit_is_num(0);
+    $cmd->stdout_is_eq("IPv4-Mapped-in-IPv6 address, using IPv4 127.0.0.1
+127.0.0.1 is alive\n");
+    $cmd->stderr_like(qr{ \[<- 127.0.0.1\]});
 }
 
 # ping 3 times 127.0.0.1
